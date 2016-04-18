e8e189268740d00bea7e68f44c8cf15fab0424b0
Devin Curry <devination3d@gmail.com>
2016-04-17 18:56:31 -0700

    Shapes: Add pinned LineSegments

--- Shapes/Sources/Project.hx
+++ Shapes/Sources/Project.hx
@@ -5,6 +5,7 @@ import kha.Scheduler;
 import kha.System;
 import kha.math.Vector2;
 import verlet.Objects.Tire;
+import verlet.Objects.LineSegments;
 import verlet.Verlet;
 import verlet.Renderer;
 
@@ -20,6 +21,8 @@ class Project {
 		verletRenderer = Renderer.Instance;
 		
 		// Entities
+		var segment = new LineSegments([new Vector2(20,10), new Vector2(40,10), new Vector2(60,10), new Vector2(80,10), new Vector2(100,10)], 0.02, [0,4]);
+		
 		new Tire(new Vector2(100,128), 64, 7, .1, .3);
 		new Tire(new Vector2(350,64), 64, 32, .03, .5);
 		new Tire(new Vector2(600,32), 50, 4, .2, .7);
