93e9e4f3109ca39c6dd16beea344fc9b9844ae45
Devin Curry - Laptop <Devination3D@gmail.com>
2016-04-19 00:59:02 -0700

    Collision: Circle vs Particle

---
+++ Collision/Sources/Project.hx
@@ -0,0 +1,43 @@
+package;
+
+import kha.Framebuffer;
+import kha.Scheduler;
+import kha.System;
+import kha.math.Vector2;
+import verlet.Objects.Tire;
+import verlet.Verlet;
+import verlet.Renderer;
+import verlet.collision.Shapes.Circle;
+
+class Project {
+	var world:Verlet;
+	var verletRenderer:Renderer;
+	
+	public function new() {
+		System.notifyOnRender(render);
+		Scheduler.addTimeTask(update, 0, 1 / 60);
+		world = new Verlet(800, 600);
+		// Render Verlet world
+		verletRenderer = Renderer.Instance;
+		
+		// Entities
+		new Tire(new Vector2(500,32), 64, 32, .05, .5);
+		
+		// Colliders
+		new Circle(new Vector2(222, 200), 256);
+		new Circle(new Vector2(666, 300), 128);
+		new Circle(new Vector2(400, 600), 128);
+	}
+
+	function update(): Void {
+		
+		world.update(10);
+	}
+
+	function render(framebuffer: Framebuffer): Void {		
+		var graphics = framebuffer.g2;
+		graphics.begin();
+		verletRenderer.render(graphics);
+		graphics.end();
+	}
+}
---
+++ Collision/khafile.js
@@ -0,0 +1,5 @@
+var project = new Project('New Project');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+project.addLibrary('verlet');
+return project;
